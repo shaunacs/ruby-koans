8857e5541c07b3df8a420c23c75181d5b86c1a25
Shauna Saunders <shaunacsaunders@outlook.com>
2023-03-02 08:19:10 -0500

    Continue about strings

--- about_strings.rb
+++ about_strings.rb
@@ -113,13 +113,13 @@ EOS
 
   def test_single_quoted_string_do_not_interpret_escape_characters
     string = '\n'
-    assert_equal __, string.size
+    assert_equal 2, string.size
   end
 
   def test_single_quotes_sometimes_interpret_escape_characters
     string = '\\\''
-    assert_equal __, string.size
-    assert_equal __, string
+    assert_equal 2, string.size
+    assert_equal "\\" + "\'", string
   end
 
   def test_double_quoted_strings_interpolate_variables
